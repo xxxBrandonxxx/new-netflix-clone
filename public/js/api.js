4e7dc902c95c1a5adea03d7f64888eab8910dbc0
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2022-10-09 18:54:33 +0200

    home page

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "995a5ac7009f5287e220a6346c03fbe6";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
